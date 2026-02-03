ce18c662f69f9f6db1c1a69a1ff0a3dd03ea05e9
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-02-02 19:45:26 -0500

    Add files via upload

---
+++ employe_detail.py
@@ -0,0 +1,7 @@
+def detail():
+    x=int(input("what are your salary expectations a month "))
+    y=int(input("how many years of experiance do you have "))
+    z=str(input("where do you live "))
+    return x,y,z
+
+print(detail())
