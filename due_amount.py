93209ba8c82d3756ce5f16e989e1ebff9ec4067c
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-02-12 20:06:28 -0500

    Add files via upload

---
+++ due_amount.py
@@ -0,0 +1,6 @@
+x=float(input("enter the amount you paid "))
+y=float(input("enter the amount you owe them "))
+
+def f(a,b):
+    return a-b
+print(f(x,y),"$ is the amount of change needed to be returned")
